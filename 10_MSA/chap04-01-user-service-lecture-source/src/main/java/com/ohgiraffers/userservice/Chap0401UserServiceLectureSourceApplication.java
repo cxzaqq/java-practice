4a9fc7e7234a871a5938ed20fa92460146087e96
cxzaqq <roy1228@naver.com>
2025-03-21 08:59:54 +0900

    update

--- 10_MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/Chap0401UserServiceLectureSourceApplication.java
+++ 10_MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/Chap0401UserServiceLectureSourceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class Chap0401UserServiceLectureSourceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Chap0401UserServiceLectureSourceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Chap0401UserServiceLectureSourceApplication.class, args);
+	}
 
 }
--- 10_MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/dto/UserDTO.java
+++ 10_MSA/chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/dto/UserDTO.java
@@ -5,6 +5,9 @@ import lombok.Data;
 @Data
 public class UserDTO {
     private String email;
+    private String name;
     private String pwd;
+
+    /* 설명. 회원가입 진행하며 추가됨 */
     private String userId;
 }
